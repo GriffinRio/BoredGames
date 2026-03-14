47fd49db20801d26790a55184e4bd3342746889b
GriffinRio <griffinator03@gmail.com>
2026-03-13 17:31:33 -0700

    Changing to 2d setting to simplify the score for learning

--- boredGames/Games/Chess/Pieces/bishop.gd
+++
@@ -1,9 +0,0 @@
-extends "res://Games/Chess/Pieces/pieces.gd"
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	set_color()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
